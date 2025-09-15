51a51ea531af30ee83c17cc5b6f1132d72c0c493
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2025-09-15 20:26:44 +0300

    Revert "winegstreamer/media-converter: Try mediaconv even if compressed output is requested."
    This reverts commit c1ee57e3d787485733ced1b16faef88de8616120.

--- dlls/winegstreamer/wg_parser.c
+++ dlls/winegstreamer/wg_parser.c
@@ -1657,7 +1657,7 @@ static GstBusSyncReply bus_handler_cb(GstBus *bus, GstMessage *msg, gpointer use
         if (gst_structure_has_name(structure, "missing-plugin"))
         {
             pthread_mutex_lock(&parser->mutex);
-            if (!parser->use_mediaconv)
+            if (!parser->use_mediaconv && !parser->output_compressed)
             {
                 GST_WARNING("Autoplugged element failed to initialise, trying again with protonvideoconvert.");
                 parser->error = true;
@@ -1894,7 +1894,7 @@ static NTSTATUS wg_parser_connect(void *args)
 
     if (ret == GST_STATE_CHANGE_FAILURE)
     {
-        if (!parser->use_mediaconv)
+        if (!parser->use_mediaconv && !parser->output_compressed)
         {
             GST_WARNING("Failed to play media, trying again with protonvideoconvert.");
             use_mediaconv = true;
@@ -1910,7 +1910,7 @@ static NTSTATUS wg_parser_connect(void *args)
         pthread_cond_wait(&parser->init_cond, &parser->mutex);
     if (parser->error)
     {
-        if (!parser->use_mediaconv)
+        if (!parser->use_mediaconv && !parser->output_compressed)
             use_mediaconv = true;
         pthread_mutex_unlock(&parser->mutex);
         goto out;
