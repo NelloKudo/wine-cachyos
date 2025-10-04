1e790134cf07b72a9dda36d58a9669025d2e17e4
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2025-10-04 16:22:32 +0300

    Reapply "HACK: steam: ntdll: Setup steamclient trampolines to lsteamclient."
    This reverts commit 69e70b32eb92fe301da863d688a21c1037ebda4b.

--- dlls/ntdll/unixlib.h
+++ dlls/ntdll/unixlib.h
@@ -66,6 +66,12 @@ struct unwind_builtin_dll_params
     CONTEXT                    *context;
 };
 
+struct steamclient_setup_trampolines_params
+{
+    HMODULE src_mod;
+    HMODULE tgt_mod;
+};
+
 struct debugstr_pc_args
 {
     void *pc;
@@ -83,6 +89,7 @@ enum ntdll_unix_funcs
     unix_wine_server_handle_to_fd,
     unix_wine_spawnvp,
     unix_system_time_precise,
+    unix_steamclient_setup_trampolines,
     unix_is_pc_in_native_so,
     unix_debugstr_pc,
 };
